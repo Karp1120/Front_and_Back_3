ddde875b8d51976259dfe07fe71c8e3a90d394d3
Sasha <antonov.05068@gmail.com>
2025-04-21 16:55:22 +0300

    добавление чата с поддрежкой

--- user/server.js
+++ user/server.js
@@ -1,25 +1,53 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = 8080;
 
 const productsPath = path.join(__dirname, '../data/products.json');
 
+const loadProducts = () => {
+  const data = fs.readFileSync(productsPath, 'utf8');
+  return JSON.parse(data);
+};
+
+const schema = buildSchema(`
+  type Product {
+    id: Int
+    name: String
+    price: Int
+    description: String
+    categories: [String]
+  }
+
+  type Query {
+    products: [Product]
+    productNames: [String]
+    productsShort: [Product]
+  }
+`);
+
+
+const root = {
+  products: () => loadProducts(),
+  productNames: () => loadProducts().map(p => p.name),
+  productsShort: () => loadProducts().map(({ name, price }) => ({ name, price }))
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true, 
+}));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/main', (req, res) => {
-    console.log("Читаем из файла:", productsPath);
-  fs.readFile(productsPath, 'utf8', (err, data) => {
-    if (err) return res.status(500).send('Ошибка чтения данных');
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  });
-});
-
 app.listen(PORT, () => {
-  console.log(`User frontend server is running at http://localhost:${PORT}`);
+  console.log(`User frontend + GraphQL сервер запущен: http://localhost:${PORT}`);
+  console.log(`GraphQL Playground: http://localhost:${PORT}/graphql`);
 });
